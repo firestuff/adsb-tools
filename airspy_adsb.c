d660117f7a727fd3843852b643ceaf0bfe47a361
Ian Gulliver <flamingcow@gmail.com>
2016-02-14 07:17:53 +0000

    Formatting

--- airspy_adsb.c
+++ airspy_adsb.c
@@ -8,7 +8,7 @@ bool airspy_adsb_parse(struct buf *buf, struct packet *packet) {
 	if (buf->length < 1 || *buf_at(buf, 0) != '*') {
 		return false;
 	}
-  if (buf->length >= 35 && *buf_at(buf, 34) == '\n') {
+	if (buf->length >= 35 && *buf_at(buf, 34) == '\n') {
 		buf_consume(buf, 35);
 		return true;
 	}
