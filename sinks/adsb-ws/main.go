c1fc51fddf78f194498fbd0c7175a5c48e17cb8e
Ian Gulliver <flamingcow@gmail.com>
2016-03-10 21:44:00 -0800

    Support serving a static directory along with the websocket stream

--- sinks/adsb-ws/main.go
+++ sinks/adsb-ws/main.go
@@ -7,13 +7,19 @@ import (
 )
 
 var bindaddr = flag.String("bind-address", ":8080", "Address to respond to HTTP requests on")
+var staticdir = flag.String("static-dir", "", "Static directory to serve at /")
 
 func main() {
 	log.SetFlags(0)
 	flag.Parse()
 	go h.run()
 	go readInput()
+
 	http.HandleFunc("/stream", serveStream)
+	if *staticdir != "" {
+		fs := http.FileServer(http.Dir(*staticdir))
+		http.Handle("/", fs)
+	}
 	err := http.ListenAndServe(*bindaddr, nil)
 	if err != nil {
 		log.Fatal("Error starting web server: ", err)
