2d1b4919b3f5122b92f78697ef988753ee2a8c8d
Ian Gulliver <flamingcow@gmail.com>
2016-03-10 16:17:47 -0800

    Start of a websocket server.

---
+++ sinks/adsb-ws/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func main() {
+	flag.Parse()
+	http.HandleFunc("/stream", serveStream)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ sinks/adsb-ws/stream.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func readPump(ws *websocket.Conn) {
+	for {
+		msgtype, msg, err := ws.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		log.Println(msgtype, msg)
+	}
+}
+
+func serveStream(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	readPump(ws)
+}
